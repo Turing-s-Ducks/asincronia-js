912da76b99d335709979e20e3fb080f1b628b7bc
Martin <alquimista3301@gmail.com>
2020-07-26 15:07:39 -0500

    Avance Generadores

---
+++ 04-generadores/01-index.js
@@ -0,0 +1,5 @@
+// https://javascript.info/async-iterators-generators
+// http://thecodebarbarian.com/async-generator-functions-in-javascript.html
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/function*
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/for-await...of
